5ca73a2ec01ad914686fa4657f82becfd18e5e16
GABRIEL SOUZA <gabrieldsml1190@gmail.com>
2024-03-08 18:56:09 -0300

    Last Change

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public class MainManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor(); 
     }
 
     // Update is called once per frame
@@ -23,4 +25,33 @@ public class MainManager : MonoBehaviour
     {
         
     }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+        {
+            SaveData data = new SaveData();
+            data.TeamColor = TeamColor;
+
+            string json = JsonUtility.ToJson(data);
+        
+            File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+        }
+
+    public void LoadColor()
+        {
+            string path = Application.persistentDataPath + "/savefile.json";
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+                TeamColor = data.TeamColor;
+            }
+        }
 }
+
